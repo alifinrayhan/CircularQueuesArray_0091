c3543c3b706af02d202b23de27cac30da4237cc2
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-31 15:18:58 +0700

    int main

--- CircullarQueuseArray.cpp
+++ CircullarQueuseArray.cpp
@@ -96,10 +96,52 @@ public:
         }
 
 
-    }
-
-    
-
-    
+    }  
 
 };
+
+int main() {
+    Queues q;
+    char ch;
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << " 1. Implement Insert operation" << endl;
+            cout << "2. Implemnet delete operation" << endl;
+            cout << "3. Display value" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch) {
+            case '1': {
+
+                q.insert();
+                break;
+            }
+            case '2': {
+                q.remove();
+                break;
+            }
+            case '3': {
+                q.display();
+                break;
+            }
+            case '4': {
+                return 0;
+            }
+            default: {
+                cout << "Invalid optional!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception& e) {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+
+    return 0;
+}
